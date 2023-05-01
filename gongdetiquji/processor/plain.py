e1461e1768da52cbb1090b601aeb3514f3b5b223
3TUSK <git@tritusk.info>
2023-04-30 19:40:19 -0700

    Try fix it again

--- gongdetiquji/processor/plain.py
+++ gongdetiquji/processor/plain.py
@@ -6,7 +6,7 @@ def process_plain_text(fname: str, content: bytes):
     if fname.endswith('txt') or fname.endswith('log'):
         decoded=try_decode(content)
         if decoded:
-            return [ (fname, decoded) ], True
+            return ([ (fname, decoded) ], True)
         else:
             logging.warn(f"Cannot guess encoding for file '{fname}', uploading cannot continue.")
-    return None, True
+    return (None, True)
